48bcf9b694f37b8bc78f03f88bf14cecf5aaea60
danielalvesmoura <danielalvesmoura2006@gmail.com>
2025-12-10 13:13:19 -0300

    refactor-global-controller

--- PortalX/src/main/java/main/controller/GlobalController.java
+++ PortalX/src/main/java/main/controller/GlobalController.java
@@ -21,53 +21,7 @@ import model.Inscricao;
 
 import java.time.LocalDate;
 
-public abstract class GlobalController<A, T> extends GlobalControllerComObjeto<A, T> {
-
-    /*
-
-    public String cssCelulaVerde =
-            "-fx-background-color: rgba(0, 255, 8, 0.2); " +
-            "-fx-background-radius: 40; " +
-            "-fx-text-fill: rgba(107, 255, 111, 1); " +
-            "-fx-font-weight: bold;" +
-            "-fx-alignment: CENTER;";
-
-    public void trocaTela(String caminho) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(caminho));
-
-        Parent pagina = loader.load();
-
-        conteudo.getChildren().clear();
-
-        conteudo.setCenter(pagina);
-    }
-
-    public void modal(String caminho) throws Exception {
-        FXMLLoader appLoader = new FXMLLoader(getClass().getResource(caminho));
-
-        Parent app = appLoader.load();
-
-        Object controller = appLoader.getController();
-
-        colocarT(null,controller);
-
-        Stage modal = new Stage();
-        modal.setTitle("Portal X");
-        modal.setScene(new Scene(app));
-
-        // Modal bloqueia interação com a janela principal
-        modal.initModality(Modality.WINDOW_MODAL);
-
-        // Define que a janela principal é a "dona" do modal
-        modal.initOwner(stage);
-
-        modal.setResizable(true);
-
-        // Abre o modal e bloqueia até fechar
-        modal.showAndWait();
-    }
-
-     */
+public abstract class GlobalController<A, T>{
 
     protected Stage stage;
 
